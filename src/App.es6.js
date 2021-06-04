da5311d0d11a0aea3ba50b1ae725fe554edea84f
Nthabi <mshnth009@myuct.ac.za>
2021-06-04 11:40:32 +0200

    can dynamically change date now

--- src/App.es6.js
+++ src/App.es6.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+    // constructor allows us to have local state in our class
     constructor(props){
         super(props);
         this.state = {
@@ -9,13 +10,14 @@ class App extends Component {
         }
     }
     updateDeadline = () => {
-        this.setState({deadline: 'November 16. 2021'});
+        // We never change state directly so we use this setState method instead of saying this.state.deadline = x
+        this.setState({deadline: this.state.newDeadline});
     }
     render() {
         return (
             <div>
                 <h5>Countdown to {this.state.deadline}</h5>
-                <input placeholder='New date'/>
+                <input placeholder='New date' onChange={e => this.setState({newDeadline: e.target.value})}/>
                 <button onClick={() => this.updateDeadline()}>Update</button>
             </div>
         )   
